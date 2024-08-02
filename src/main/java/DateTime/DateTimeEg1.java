f8842efde4d33609bdc3e346697e2ad7b432d98b
Kumbhare <Kunal.Kumbhare@saint-gobain.com>
2024-08-02 19:10:33 +0530

    Date Package is created and also done 1 example

--- src/main/java/DateTime/DateTimeEg1.java
+++ src/main/java/DateTime/DateTimeEg1.java
@@ -1,5 +1,27 @@
 package DateTime;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class DateTimeEg1 {
 
+    public static void main(String[] args) {
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println("Before Formatting : "+dt);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String FormatedDate = dt.format(dtf);
+        System.out.println("After Formatting : "+FormatedDate);
+
+        LocalDate date = LocalDate.now();
+        System.out.println("Date : "+date);
+
+        Date d1 = new Date();
+        System.out.println(d1);
+
+    }
 }
